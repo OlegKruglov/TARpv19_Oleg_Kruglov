8a3e6395dda4a28555b81bd5ba63a3a08bb75859
алекс <@DESKTOP-TVQ711I>
2020-03-31 15:40:47 +0300

    Ask User Name

--- TARpv19_Oleg_Kruglov/Program.cs
+++ TARpv19_Oleg_Kruglov/Program.cs
@@ -10,8 +10,13 @@ namespace TARpv19_Oleg_Kruglov
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!");
+            Console.WriteLine("Please, enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name +"!");
+
             Console.ReadLine();
+            
         }
     }
 }

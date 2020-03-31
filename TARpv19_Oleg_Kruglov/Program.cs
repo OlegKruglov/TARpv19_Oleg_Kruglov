dc3e3675a78a779c763b6f4fc84541758c417bd8
алекс <@DESKTOP-TVQ711I>
2020-03-31 15:31:35 +0300

    Create Hello App

---
+++ TARpv19_Oleg_Kruglov/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARpv19_Oleg_Kruglov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            Console.ReadLine();
+        }
+    }
+}
